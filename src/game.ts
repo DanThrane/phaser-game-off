e17b369e4321e88079e822138b74bd8fb55959ee
Dan Sebastian Thrane <dthrane@gmail.com>
2018-11-10 10:48:17 +0100

    Automatic resizing of game. Changes aspect ratio from 4:3 to 16:9

--- src/game.ts
+++ src/game.ts
@@ -6,9 +6,9 @@ import { WorldScene } from "./scenes/world.scene";
 // import { BootScene } from "./scenes/BootScene";
 
 const config: GameConfig = {
-  version: "0.1,0",
-  width: 800,
-  height: 600,
+  version: "0.1.0",
+  width: 1280,
+  height: 720,
   type: Phaser.AUTO,
   parent: "game",
   scene: [WorldScene],
@@ -37,5 +37,27 @@ export class Game extends Phaser.Game {
 }
 
 window.onload = () => {
-  var game = new Game(config);
+  const game = new Game(config);
+
+  const desiredRatio = 16 / 9;
+  const inverseRatio = 1 / desiredRatio;
+  const resize = () => {
+    const canvas = game.canvas;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    const heightBasedOnWidth = width * inverseRatio;    
+    const widthBasedOnHeight = height * desiredRatio;
+
+    if (heightBasedOnWidth <= height) {
+      canvas.style.height = `${heightBasedOnWidth}px`;
+      canvas.style.width = `${width}px`;
+    } else {
+      canvas.style.width = `${widthBasedOnHeight}px`;
+      canvas.style.height = `${height}px`;
+    }
+  };
+
+  window.addEventListener('resize', resize);
+  resize();
 };
