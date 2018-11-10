1a343bf535cc5a8fba73baac6c764afefa77ef65
Dan Sebastian Thrane <dthrane@gmail.com>
2018-11-10 11:50:41 +0100

    Proof of concept for tiled

--- src/scenes/world.scene.ts
+++ src/scenes/world.scene.ts
@@ -5,15 +5,15 @@ export class WorldScene extends Phaser.Scene {
 	private map?: Phaser.Tilemaps.Tilemap = undefined;
 	private tileset?: Phaser.Tilemaps.Tileset = undefined;
 	private world = {
-		width: 3200,
+		width: 32000,
 		height: 3200
 	};
 	private player!: Player;
-	
+
 	constructor() {
-	  super({
-		key: "WorldScene"
-	  });
+		super({
+			key: "WorldScene"
+		});
 	}
 
 	preload() {
@@ -23,25 +23,14 @@ export class WorldScene extends Phaser.Scene {
 			"assets/dude.png",
 			{ frameWidth: 32, frameHeight: 48 }
 		);
+		this.load.tilemapTiledJSON("tiled-map", "assets/tiled-test.json");
 	}
 
 	create() {
 		// Setup tilemap
-		this.map = this.make.tilemap({
-			tileWidth: 16,
-			tileHeight: 16,
-			width: this.world.width,
-			height: this.world.height
-		});
-		this.tileset = this.map.addTilesetImage("tiles", "tiles", 16, 16);
-		let layer = this.map.createBlankDynamicLayer("layer 1", this.tileset, this.map.width, this.map.height, this.map.tileWidth, this.map.tileHeight);
-		
-		let worldGen = new WorldGenerator(this.map, "hugeRoom", {
-			tileWidth: 16,
-			tileHeight: 16,
-		});
-		worldGen.buildLayout();
-		// layer.randomize(0, 0, this.map.width, this.map.height, [0, 1, 2]); // <- try this out instaead :)
+		this.map = this.add.tilemap("tiled-map");
+		this.tileset = this.map.addTilesetImage("tiles");
+		this.map.createStaticLayer("Tile Layer 1", this.tileset, 0, 0);
 
 		this.player = new Player(this, 0, 0, "dude");
 		this.player.x = this.map.tileToWorldX(0);
