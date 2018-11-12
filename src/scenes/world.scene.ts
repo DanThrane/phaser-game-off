ef2e456b684ce92ca27f254bbf3a37ee814da324
Jonas Malte Hinchely <jonas@hinchely.dk>
2018-11-12 18:13:48 +0100

    Add collision between player and wall

--- src/scenes/world.scene.ts
+++ src/scenes/world.scene.ts
@@ -28,13 +28,19 @@ export class WorldScene extends Phaser.Scene {
 
 	create() {
 		// Setup tilemap
-		this.map = this.add.tilemap("tiled-map");
+		this.map = this.make.tilemap({ key: "tiled-map", tileHeight: 32, tileWidth: 32});
 		this.tileset = this.map.addTilesetImage("tiles");
-		this.map.createStaticLayer("Tile Layer 1", this.tileset, 0, 0);
+		const layer = this.map.createStaticLayer("Tile Layer 1", this.tileset, 0, 0);
+		
+		// Set collision for Tile items 2 - 2 (inclusive) 
+		this.map.setCollisionBetween(2, 2);
 
 		this.player = new Player(this, 0, 0, "dude");
 		this.player.x = this.map.tileToWorldX(0);
 		this.player.y = this.map.tileToWorldY(0);
+
+		// Add collider between collision tile items and player
+		this.physics.add.collider(layer, this.player)
 
 		// Scroll to the player		
 		let cam = this.cameras.main;
